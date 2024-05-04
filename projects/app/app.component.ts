32c7f305b04b20e0daa16934e3d709798eed6e93
Николай <kolyatola@mail.ru>
2024-05-04 13:48:29 +0300

    added and test first component and rebuild storybook - config

--- projects/app/app.component.ts
+++ projects/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavPanelComponent} from "../ui/src/lib/components/nav-panel/nav-panel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavPanelComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ projects/ui/src/lib/components/task-create-sample/task-create-sample.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-create-sample',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-create-sample.component.html',
+  styleUrl: './task-create-sample.component.scss'
+})
+export class TaskCreateSampleComponent {
+
+}
