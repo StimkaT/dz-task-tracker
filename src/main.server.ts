a221dc4daa1ed1af22adc16ab0ae3884c335ece1
Николай <kolyatola@mail.ru>
2024-05-04 12:18:29 +0300

    1h project. start

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
+import { AppComponent } from '../projects/app/app.component';
+import { config } from '../projects/app/app.config.server';
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
